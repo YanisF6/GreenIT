3511f8560a1f40aad18cbc4d3926f0df6cba1022
t.johan14 <johan.travert@aliceadsl.fr>
2022-03-20 23:17:33 +0100

    affichage des scores

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 
 import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
@@ -39,12 +39,6 @@ const config = {
   alwaysSearchOnInitialLoad: true
 };
 
-function submitSearch(e) {
-  e.preventDefault();
-  let elt = document.getElementsByClassName('sui-layout-body');
-  elt[0].style.display = 'block';
-}
-
 let scrollerButton = document.getElementsByClassName('arrow');
 window.onscroll = function(e) {
   if (this.pageYOffset < 300) {
@@ -54,7 +48,133 @@ window.onscroll = function(e) {
   }
 };
 
+const departements = [
+  'Ain',
+  'Aisne',
+  'Allier',
+  'Alpes-de-Haute-Provence',
+  'Hautes-Alpes',
+  'Alpes-Maritimes',
+  'Ardèche',
+  'Ardennes',
+  'Ariège',
+  'Aube',
+  'Aude',
+  'Aveyron',
+  'Bouches-du-Rhône',
+  'Calvados',
+  'Cantal',
+  'Charente',
+  'Charente-Maritime',
+  'Cher',
+  'Corrèze',
+  'Corse-du-Sud',
+  'Haute-Corse',
+  'Côte-d\'Or',
+  'Côtes d\'Armor',
+  'Creuse',
+  'Dordogne',
+  'Doubs',
+  'Drôme',
+  'Eure',
+  'Eure-et-Loir',
+  'Finistère',
+  'Gard',
+  'Haute-Garonne',
+  'Gers',
+  'Gironde',
+  'Hérault',
+  'Ille-et-Vilaine',
+  'Indre',
+  'Indre-et-Loire',
+  'Isère',
+  'Jura',
+  'Landes',
+  'Loir-et-Cher',
+  'Loire',
+  'Haute-Loire',
+  'Loire-Atlantique',
+  'Loiret',
+  'Lot',
+  'Lot-et-Garonne',
+  'Lozère',
+  'Maine-et-Loire',
+  'Manche',
+  'Marne',
+  'Haute-Marne',
+  'Mayenne',
+  'Meurthe-et-Moselle',
+  'Meuse',
+  'Morbihan',
+  'Moselle',
+  'Nièvre',
+  'Nord',
+  'Oise',
+  'Orne',
+  'Pas-de-Calais',
+  'Puy-de-Dôme',
+  'Pyrénées-Atlantiques',
+  'Hautes-Pyrénées',
+  'Pyrénées-Orientales',
+  'Bas-Rhin',
+  'Haut-Rhin',
+  'Rhône',
+  'Haute-Saône',
+  'Saône-et-Loire',
+  'Sarthe',
+  'Savoie',
+  'Haute-Savoie',
+  'Paris',
+  'Seine-Maritime',
+  'Seine-et-Marne',
+  'Yvelines',
+  'Deux-Sèvres',
+  'Somme',
+  'Tarn',
+  'Tarn-et-Garonne',
+  'Var',
+  'Vaucluse',
+  'Vendée',
+  'Vienne',
+  'Haute-Vienne',
+  'Vosges',
+  'Yonne',
+  'Territoire de Belfort',
+  'Essonne',
+  'Hauts-de-Seine',
+  'Seine-St-Denis',
+  'Val-de-Marne',
+  'Val-D\'Oise',
+  'Guadeloupe',
+  'Martinique',
+  'Guyane',
+  'La Réunion',
+  'Mayotte',
+]
+
 export default function App() {
+  const [list, setList] = useState([]);
+
+  function submitSearch(e) {
+    e.preventDefault();
+    //let elt = document.getElementsByClassName('sui-layout-body');
+    let results = document.getElementsByClassName('sui-result__details');
+    let lis = results[0].getElementsByTagName("li");
+    let values = [];
+    for (var i = 0; i < lis.length; ++i) {
+      values.push(lis[i].children[1].innerHTML);
+    }
+    let array = [
+      { name: 'Score Global', ville: values[2], departement: values[0], region: values[17], score_ville: lis[10], score_departement: values[12], score_region: values[1]},
+      { name: 'Accès aux interfaces numériques', ville: values[2], departement: values[10], region: values[17], score_ville: values[13], score_departement: values[18], score_region: values[11]},
+      { name: 'Accès à l\'information', ville: values[2], departement: values[0], region: values[17], score_ville: values[14], score_departement: values[4], score_region: values[8]},
+      { name: 'Compétences Numériques', ville: values[2], departement: values[0], region: values[17], score_ville: values[5], score_departement: values[15], score_region: values[6]},
+      { name: 'Compétences Administratives', ville: values[2], departement: values[0], region: values[17], score_ville: values[3], score_departement: values[7], score_region: values[9]}
+    ];
+    setList(list => [...list, array]);
+    console.log(list)
+    //elt[0].style.display = 'block';
+  }
 
   return (
     <div className="App">
@@ -74,7 +194,7 @@ export default function App() {
           <a href="#inputbloc"><div className="arrow"></div></a>
         </div>
         <div id='inputbloc'></div>
-        <hr></hr>
+        <hr className="main-hr"></hr>
         <h2>Recherchez votre commune</h2>
         <div className="input-bloc">
           <SearchProvider config={config}>
@@ -101,6 +221,30 @@ export default function App() {
             </WithSearch>
           </SearchProvider>
           <span className="submit-button" onClick={submitSearch}>CHERCHER</span>
+        </div>
+        <div className="scores-block">
+          {list.map((item) => (
+            <div key={item.name}>
+              <h3>{ item.name }</h3>
+              { item.name == "Score Global" && <p>description 1</p> }
+              { item.name == "Accès aux interfaces numériques" && <p>description 2</p> }
+              { item.name == "Accès à l\'information" && <p>description 3</p> }
+              { item.name == "Compétences Numériques" && <p>description 4</p> }
+              { item.name == "Compétences Administratives" && <p>description 5</p> }
+              <table className="details-score">
+                <tr className="scores-title">
+                  <td>{ item.ville }</td>
+                  <td>{ departements[item.departement] }</td>
+                  <td>{ item.region }</td>
+                </tr>
+                <tr className="scores-number">
+                  <td>{ item.score_ville }</td>
+                  <td>{ item.score_departement }</td>
+                  <td>{ item.score_region }</td>
+                </tr>
+              </table>
+            </div>
+          ))}
         </div>
       </div>
     </div>
